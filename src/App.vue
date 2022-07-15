158b90a8e136b0a27a87d2c5e54700b930d32e93
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-07-16 01:18:07 +0900

    移動処理実装

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,49 @@ export default {
   name: 'App',
   data: function () {
     return {
+      canvas: null,
       context: null,
+      x: 0,
+      y: 0,
     }
   },
   mounted: function () {
-    const canvas = this.$refs.canvas
-    if (!canvas || !canvas.getContext) {
+    this.canvas = this.$refs.canvas
+    if (!this.canvas || !this.canvas.getContext) {
       alert('Contextが取得できませんでした')
       return
     }
-    this.context = canvas.getContext('2d')
-    this.context.strokeRect(100, 50, 120, 60)
+    this.context = this.canvas.getContext('2d')
+    setInterval(this.drawFrame, 60 / 1000)
+    document.addEventListener('keydown', this.onKeyDown)
   },
   methods: {
+    /**
+     * １フレーム分の描画処理
+     */
+    drawFrame: function () {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      this.context.strokeRect(this.x, this.y, 64, 64)
+    },
+    /**
+     * キーが押された
+     */
+    onKeyDown: function (e) {
+      switch (e.key) {
+        case 'ArrowLeft':
+          this.x -= 10
+          break
+        case 'ArrowRight':
+          this.x += 10
+          break
+        case 'ArrowUp':
+          this.y -= 10
+          break
+        case 'ArrowDown':
+          this.y += 10
+          break
+      }
+    }
   },
 }
 </script>
