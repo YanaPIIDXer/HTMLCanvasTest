7745dd6d845b35993e3586aa04d59713512a8b12
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-07-16 01:11:41 +0900

    とりあえず描画してみた

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
+    <canvas ref="canvas" width="1024" height="768">
+      Canvas非対応
+    </canvas>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {
+  data: function () {
+    return {
+      context: null,
+    }
+  },
+  mounted: function () {
+    const canvas = this.$refs.canvas
+    if (!canvas || !canvas.getContext) {
+      alert('Contextが取得できませんでした')
+      return
+    }
+    this.context = canvas.getContext('2d')
+    this.context.strokeRect(100, 50, 120, 60)
+  },
+  methods: {
   },
 }
 </script>
